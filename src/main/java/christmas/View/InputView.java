d57a21d87a4cdfd15b9ea2d79ba6492a5296c6bc
HurDong <gjehdals456@gmail.com>
2023-11-15 06:41:27 +0900

    Feat: InputView

---
+++ src/main/java/christmas/View/InputView.java
@@ -0,0 +1,17 @@
+package christmas.View;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public static String readDate() {
+        String input = Console.readLine();
+        return input;
+    }
+
+    public static String readOrder() {
+        String input = Console.readLine();
+        return input;
+    }
+
+}
